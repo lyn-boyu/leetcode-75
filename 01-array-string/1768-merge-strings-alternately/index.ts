aa0b059560e37291bade8ac38778cf558907995f
jason-l <linc.3395@gmail.com>
2024-06-11 18:14:48 -0700

    feat: add 1768 answer

--- 01-array-string/1768-merge-strings-alternately/index.ts
+++ 01-array-string/1768-merge-strings-alternately/index.ts
@@ -29,5 +29,12 @@ merged: a p  b  q  c   d
 
 
 export function mergeAlternately(word1: string, word2: string): string {
-    return ''
+    const maxLength = Math.max(word1.length, word2.length);
+    let ans = ''
+    for (let i = 0; i < maxLength; i++) {
+        const firstLetter = word1[i] ?? ''
+        const secondLetter = word2[i] ?? ''
+        ans += firstLetter + secondLetter
+    }
+    return ans
 };
