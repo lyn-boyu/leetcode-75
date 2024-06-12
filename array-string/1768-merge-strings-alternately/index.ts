a5f48dae426b949dcc8cf7ac68ef8da60d17ccd6
jason-l <linc.3395@gmail.com>
2024-06-11 18:04:04 -0700

    feat: add 1768 merge-string-alternately - Easy

--- array-string/1768-merge-strings-alternately/index.ts
+++ array-string/1768-merge-strings-alternately/index.ts
@@ -1,10 +1,33 @@
+/*
+1768. Merge Strings Alternately  - Easy
+
+Example 1:
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:  p   q   r
+merged: a p b q c r
+
+
+Example 2:
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:  p   q   r   s
+merged: a p b q r   s
+
+Example 3:
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a    b     c   d
+word2:  p    q 
+merged: a p  b  q  c   d
+*/
+
+
 export function mergeAlternately(word1: string, word2: string): string {
-    const maxLength = Math.max(word1.length, word2.length);
-    let ans = ''
-    for (let i = 0; i < maxLength; i++) {
-        const firstLetter = word1[i] ?? ''
-        const secondLetter = word2[i] ?? ''
-        ans += firstLetter + secondLetter
-    }
-    return ans
+    return ''
 };
