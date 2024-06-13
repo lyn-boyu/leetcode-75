7a22c4fd5df5fb9aba957c7d24f28ade3a14c6a0
jason-l <linc.3395@gmail.com>
2024-06-13 12:11:29 -0700

    feat: add problem 2352 equal-row-and-column-pairs

---
+++ hash-map-and-set/2352-equal-row-and-column-pairs/index.ts
@@ -0,0 +1,52 @@
+/**
+ 
+2352. Equal Row and Column Pairs - Medium
+
+
+Given a 0-indexed n x n integer matrix grid, 
+return the number of pairs (ri, cj) such that row ri and column cj are equal.
+
+A row and column pair is considered equal 
+if they contain the same elements in the same order (i.e., an equal array).
+
+ 
+
+Example 1:
+--------------------------------
+3 2 1
+1 7 6
+2 7 7
+--------------------------------
+Input: grid = [[3,2,1],[1,7,6],[2,7,7]]
+Output: 1
+Explanation: There is 1 equal row and column pair:
+- (Row 2, Column 1): [2,7,7]
+
+Example 2:
+--------------------------------
+3 1 2 2
+1 4 4 5
+2 4 2 2
+2 4 2 2
+--------------------------------
+Input: grid = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]
+Output: 3
+Explanation: There are 3 equal row and column pairs:
+- (Row 0, Column 0): [3,1,2,2]
+- (Row 2, Column 2): [2,4,2,2]
+- (Row 3, Column 2): [2,4,2,2]
+ 
+
+Constraints:
+n == grid.length == grid[i].length
+1 <= n <= 200
+1 <= grid[i][j] <= 105
+
+*/
+
+function equalPairs(grid: number[][]): number {
+    return 0
+};
+
+export { equalPairs as solution };
+
