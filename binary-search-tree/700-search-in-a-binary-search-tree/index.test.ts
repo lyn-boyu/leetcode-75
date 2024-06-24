e8c5fc71a154b16d30d9bc10cf15828bc19d5d03
jason-l <linc.3395@gmail.com>
2024-06-24 16:29:24 -0700

    feat: add problem 700 search-in-a-binary-search-tree - easy

---
+++ binary-search-tree/700-search-in-a-binary-search-tree/index.test.ts
@@ -0,0 +1,52 @@
+/**
+ 
+700. Search in a Binary Search Tree - Easy
+
+https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+You are given the root of a binary search tree (BST) and an integer val.
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node.
+If such a node does not exist, return null.
+
+ 
+
+Example 1:
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+Example 2:
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+ 
+
+Constraints:
+The number of nodes in the tree is in the range [1, 5000].
+1 <= Node.val <= 107
+root is a binary search tree.
+1 <= val <= 107
+
+*/
+
+
+
+import { expect, test, describe } from "bun:test";
+import { solution } from './index'
+import { BinaryTreeNode } from "../../common/binary-tree";
+
+describe("700. Search in a Binary Search Tree - Easy", () => {
+
+    test("Input1: root = [4,2,7,1,3], val = 2", () => {
+        const root = BinaryTreeNode.createFromArray([4, 2, 7, 1, 3]);
+        const val = 2;
+        const result = solution(root, val);
+        expect(BinaryTreeNode.toArray(result)).toEqual([2, 1, 3]);
+    });
+
+    test("Input2: root = [4,2,7,1,3], val = 5 ", () => {
+        const root = BinaryTreeNode.createFromArray([4, 2, 7, 1, 3]);
+        const val = 5;
+        const result = solution(root, val);
+        expect(BinaryTreeNode.toArray(result)).toEqual([]);
+    });
+
+});
