97655f418dd041dd658aad10a812a42c339cfa7f
jason-l <linc.3395@gmail.com>
2024-07-03 11:32:26 -0700

    feat: add solution for 2542

--- 14-heap-priority-queue/2336-smallest-number-in-infinite-set/index.ts
+++ 14-heap-priority-queue/2336-smallest-number-in-infinite-set/index.ts
@@ -47,16 +47,125 @@ At most 1000 calls will be made in total to popSmallest and addBack.
  */
 
 
+class MinHeap {
+    // store data
+    heap: number[] = []
+    // calc Parent Idx
+    getParentIdx = (idx: number) => Math.floor((idx - 1) / 2)
+    // calc child Idx
+    getLeftChildIdx = (idx: number) => idx * 2 + 1
+    // calc child Idx
+    getRightChildIdx = (idx: number) => idx * 2 + 2
+    // calc Parent value
+    getParent = (idx: number) => this.heap[this.getParentIdx(idx)]
+    // get left child value
+    getLeftChild = (idx: number) => this.heap[this.getLeftChildIdx(idx)]
+    // get right child value
+    getRightChild = (idx: number) => this.heap[this.getRightChildIdx(idx)]
+    // swap
+    swap = (idx1: number, idx2: number) => {
+        [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]]
+    }
+    // move the newly added element up to maintain the heap property
+    heapifyUp = () => {
+        let idx = this.heap.length - 1
+        while (idx > 0 && this.getParent(idx) > this.heap[idx]) {
+            this.swap(this.getParentIdx(idx), idx)
+            idx = this.getParentIdx(idx)
+        }
+    }
+    // move the top element down to maintain the heap property
+    heapifyDown = () => {
+        let idx = 0
+        while (this.getLeftChildIdx(idx) < this.heap.length) {
+            //  find smaller child in child nodes
+            let smallerChildIdx = this.getLeftChildIdx(idx)
+            if (this.getRightChildIdx(idx) < this.heap.length
+                && this.getRightChild(idx) < this.getLeftChild(idx)
+            ) {
+                smallerChildIdx = this.getRightChildIdx(idx)
+            }
+
+            // break condition: upper element is no bigger than the child
+            if (this.heap[idx] <= this.heap[smallerChildIdx]) {
+                break;
+            }
+
+            // swap the current element with the smaller child
+            this.swap(idx, smallerChildIdx)
+            idx = smallerChildIdx
+        }
+    }
+    // insert and maintain the heap
+    insert = (ele: number) => {
+        this.heap.push(ele)
+        this.heapifyUp()
+    }
+    // remove and return the minimun number 
+    extractMin = (): number | null => {
+        if (this.heap.length === 0) {
+            return null
+        }
+        if (this.heap.length === 1) {
+            return this.heap.pop()!
+        }
+        // remove th
+        const min = this.heap[0]
+        this.heap[0] = this.heap.pop()!
+        this.heapifyDown()
+        return min
+    }
+    // return the size
+    size() {
+        return this.heap.length
+    }
+    // peek the value
+    peek() {
+        return this.heap[0] ?? null
+    }
+}
+
 export class SmallestInfiniteSet {
-    constructor() {
+    // Min-heap to store the current smallest positive numbers
+    private minHeap = new MinHeap();
 
-    }
+    // Set to keep track of numbers that have been popped
+    private poppedNumbers = new Set<number>();
 
+    // Array to keep track of the current range of positive numbers [currentMin, +Infinity]
+    private currentRange = [1, Number.POSITIVE_INFINITY];
+
+    // Pop the smallest number from the set
     popSmallest(): number {
-        return -1
+        // Check if the heap is empty and insert the next positive number if needed
+        if (this.minHeap.size() === 0) {
+            const next = this.getNextPositiveNum();
+            this.minHeap.insert(next);
+        }
+
+        // Extract the smallest number from the heap
+        const min = this.minHeap.extractMin();
+        this.poppedNumbers.add(min!);
+        return min!;
     }
 
-    addBack(num: number): void {
+    // Get the next positive number from the current range
+    private getNextPositiveNum(): number {
+        const [min] = this.currentRange;
+        this.currentRange[0] += 1;
+        return min;
+    }
 
+    // Add back a previously popped number to the set
+    addBack(num: number): void {
+        // Ignore if the number is bigger than or equal to the current smallest positive number
+        if (num >= this.currentRange[0]) {
+            return;
+        }
+        // Add the number back to the heap if it was previously popped
+        if (this.poppedNumbers.has(num)) {
+            this.minHeap.insert(num);
+            this.poppedNumbers.delete(num);
+        }
     }
 }
