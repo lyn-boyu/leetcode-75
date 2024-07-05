90f158e7a4100903c4e98253a02c0af910c19d46
jason-l <linc.3395@gmail.com>
2024-07-04 18:24:21 -0700

    feat: add problem 374 guess-number-higher-or-lower - easy

---
+++ 15-binary-search/374-guess-number-higher-or-lower/index.test.ts
@@ -0,0 +1,66 @@
+/**
+374. Guess Number Higher or Lower - Easy
+
+https://leetcode.com/problems/guess-number-higher-or-lower/
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+- -1: Your guess is higher than the number I picked (i.e. num > pick).
+- 1: Your guess is lower than the number I picked (i.e. num < pick).
+- 0: your guess is equal to the number I picked (i.e. num == pick).
+
+Return the number that I picked.
+
+ 
+Example 1:
+Input: n = 10, pick = 6
+Output: 6
+
+Example 2:
+Input: n = 1, pick = 1
+Output: 1
+
+Example 3:
+Input: n = 2, pick = 1
+Output: 1
+ 
+
+Constraints:
+1 <= n <= 231 - 1
+1 <= pick <= n
+*/
+
+import { expect, test, describe } from "bun:test";
+import { solution } from './index'
+
+
+describe("374. Guess Number Higher or Lower - Easy", () => {
+
+    test('Input1:  n = 10, pick = 6', () => {
+        const n = 10
+        const pick = 6
+        const result = solution(n);
+        expect(result).toBe(pick);
+    });
+
+    test('Input2:  n = 1, pick = 1', () => {
+        const n = 1
+        const pick = 1
+        const result = solution(n);
+        expect(result).toBe(pick);
+    });
+
+    test('Input3: n = 2, pick = 1', () => {
+        const n = 1
+        const pick = 1
+        const result = solution(n);
+        expect(result).toBe(pick);
+    });
+
+
+});
---
+++ 15-binary-search/374-guess-number-higher-or-lower/index.ts
@@ -0,0 +1,52 @@
+/**
+374. Guess Number Higher or Lower - Easy
+
+https://leetcode.com/problems/guess-number-higher-or-lower/
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+- -1: Your guess is higher than the number I picked (i.e. num > pick).
+- 1: Your guess is lower than the number I picked (i.e. num < pick).
+- 0: your guess is equal to the number I picked (i.e. num == pick).
+
+Return the number that I picked.
+
+ 
+Example 1:
+Input: n = 10, pick = 6
+Output: 6
+
+Example 2:
+Input: n = 1, pick = 1
+Output: 1
+
+Example 3:
+Input: n = 2, pick = 1
+Output: 1
+ 
+
+Constraints:
+1 <= n <= 231 - 1
+1 <= pick <= n
+*/
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+
+function guessNumber(n: number): number {
+    return -1
+};
+
+
+export { guessNumber as solution }
