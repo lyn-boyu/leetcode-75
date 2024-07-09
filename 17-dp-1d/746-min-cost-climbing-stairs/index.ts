7d793a338d877d37e1560e8fcfeb9c9b49d9ce7b
jason-l <linc.3395@gmail.com>
2024-07-09 11:34:33 -0700

    feat: add solution for 746

--- 17-dp-1d/746-min-cost-climbing-stairs/index.ts
+++ 17-dp-1d/746-min-cost-climbing-stairs/index.ts
@@ -37,7 +37,44 @@ Constraints:
 */
 
 function minCostClimbingStairs(cost: number[]): number {
-    return -1
+    const topIdx = cost.length
+    // Step1: Define the state.
+    // dp[i] represents the min cost of current stair
+    const dp = new Array(cost.length + 1).fill(0);
+
+    // Step2: Initialize the base cases.
+    // can start as index 1 or 0
+    // can climb one or two steps which means the initial cost is 0
+    dp[0] = 0
+    dp[1] = 0
+    // dp[2] = Math.min(0+15,0+10) = 10
+    // dp[3] = Math.min(10+20,0+15) = 15 
+
+    // Step 3: Fill the table according to the state transition equation.
+    // reach the top means stairs.length + 1
+    for (let i = 2; i <= topIdx; i++) {
+        // Step 4: Determine the state transition equation.
+        const climbOneStepCost = dp[i - 1] + cost[i - 1]
+        const climbTwoStepCost = dp[i - 2] + cost[i - 2]
+        dp[i] = Math.min(climbOneStepCost, climbTwoStepCost)
+    }
+
+    // Step 5: Construct the result using the DP table.
+    return dp[topIdx]
 };
 
 export { minCostClimbingStairs as solution }
+
+/**
+ 
+# Time Complexity
+the total time complexity is \(O(n)\).
+- **Initialization of the dp array:**  This takes \(O(n+1)\) time, where \(n\) is the length of the `cost` array.
+- **Filling the dp array:**  The loop runs from 2 to `topIdx` (which is \(n\)), and each iteration involves constant time operations. Thus, this takes \(O(n)\) time.
+
+
+# Space Complexity
+- **dp array:** The space used by the dp array is \(O(n+1)\).
+Thus, the space complexity is \(O(n)\).
+
+ */
