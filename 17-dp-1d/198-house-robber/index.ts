9bf29552f85b3e1a080bf1f57f245895a9f99a13
jason-l <linc.3395@gmail.com>
2024-07-09 12:15:50 -0700

    feat: add solution for 198

--- 17-dp-1d/198-house-robber/index.ts
+++ 17-dp-1d/198-house-robber/index.ts
@@ -32,7 +32,41 @@ Constraints:
 */
 
 function rob(nums: number[]): number {
-    return -1
+    const n = nums.length;
+    if (n === 0) return 0;
+    if (n === 1) return nums[0];
+    if (n === 2) return Math.max(nums[0], nums[1]);
+    if (n === 3) return Math.max(nums[0] + nums[2], nums[1]);
+
+
+    // Step1: Define the state.
+    const dp = new Array(nums.length).fill(0)
+
+    // Step2: Initialize the base cases.
+    dp[0] = nums[0] ?? 0
+    dp[1] = nums[1] ?? 0
+    dp[2] = (nums[2] ?? 0) + nums[0]
+
+
+    // Step 3: Fill the table according to the state transition equation.   
+    for (let i = 3; i < nums.length; i++) {
+        // Step 4: Determin the state transition equation
+        // avoid adjecent house
+        dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 3] + nums[i])
+    }
+
+    // Step 5: Construct the result using DP table
+    return Math.max(dp[nums.length - 1], dp[nums.length - 2])
+
 };
 
 export { rob as solution }
+
+/**
+ # Time Complexity: O(n)
+  -  O(n): new Array(nums.length).fill(0)
+  -  O(n): for (let i = 2; i < nums.length; i++)
+
+ # Space: Complexity: O(n)
+  - O(n): const dp = new Array(nums.length).fill(0)
+ */
