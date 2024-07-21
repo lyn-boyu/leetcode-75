d87168534af0d4edf2cceb2967ee495530666695
jason-l <linc.3395@gmail.com>
2024-07-20 22:57:08 -0700

    feat: add solution for 1071

--- 01-array-string/1071-greatest-common-divisor-of-string/index.ts
+++ 01-array-string/1071-greatest-common-divisor-of-string/index.ts
@@ -31,5 +31,39 @@ str1 and str2 consist of English uppercase letters.
 
 
 export function gcdOfStrings(str1: string, str2: string): string {
-    return ''
+    // a is bigger, b is smaller
+    const gcd = (a: number, b: number) => {
+        while (b !== 0) {
+            const temp = b
+            // b is the remainder of a % b
+            b = a % b;
+            // assign bigger value to a
+            a = temp
+        }
+        return a
+    }
+
+    if (str1 + str2 !== str2 + str1) {
+        return ''
+    }
+
+    const idx = str1.length > str2.length ? gcd(str1.length, str2.length) : gcd(str2.length, str1.length)
+    return str1.slice(0, idx)
 };
+
+
+export function solution1(str1: string, str2: string): string {
+    let temp = ''
+    let gst = ''
+
+    const isGcd = (gcd: string, str: string) => str.replaceAll(gcd, '').length === 0
+
+    for (let char of str1) {
+        temp += char
+        if (isGcd(temp, str1) && isGcd(temp, str2)) {
+            gst = temp
+        }
+    }
+    // if cannot find any gst return empty string
+    return gst
+}
