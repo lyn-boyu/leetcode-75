f52a9995b5ec4aabf2c84df570e5dd07dfe55f2a
jason-l <linc.3395@gmail.com>
2024-07-23 13:22:33 -0700

    feat: add solution for 283

--- 02-two-pointer/283-move-zeroes/index.ts
+++ 02-two-pointer/283-move-zeroes/index.ts
@@ -25,10 +25,46 @@ Constraints:
 */
 
 /** Do not return anything, modify nums in-place instead. */
+
 export function moveZeroes(nums: number[]): void {
+
+    let lastNonZeroIndex = 0; // Index of the last non-zero element found
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[lastNonZeroIndex] = nums[i];
+            lastNonZeroIndex++
+        }
+    }
+
+    for (let i = lastNonZeroIndex; i < nums.length; i++) {
+        nums[i] = 0
+    }
 
 };
 
+export function whileSolution(nums: number[]): void {
+
+    let i = 0
+    let zeroCount = 0
+
+    while (i < nums.length) {
+        if (nums[i] === 0) {
+            // remove the zero from array
+            nums.splice(i, 1)
+            // push zero to the end
+            zeroCount++
+        } else {
+            i++
+        }
+    }
+
+    // padding end with zero 
+    while (zeroCount > 0) {
+        nums.push(0)
+        zeroCount--
+    }
+}
 
 
 
