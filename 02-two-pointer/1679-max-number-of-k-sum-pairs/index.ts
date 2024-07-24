9fed40ab56ef3c1181049dcc5d99eac9fd664111
jason-l <linc.3395@gmail.com>
2024-07-24 15:51:56 -0700

    feat: add solution for 1679

--- 02-two-pointer/1679-max-number-of-k-sum-pairs/index.ts
+++ 02-two-pointer/1679-max-number-of-k-sum-pairs/index.ts
@@ -33,7 +33,81 @@ Constraints:
 
 */
 
+// BruteForce, Time complexity: O(n^2)
+export function bruteForceSolution(nums: number[], k: number): number {
+    let count = 0
+    let firstIdx = 0
+    let secondIdx = 0
+
+    while (firstIdx < nums.length) {
+        const firstNum = nums[firstIdx]
+        const target = k - firstNum
+        secondIdx = firstIdx + 1;
+        let find = false
+        for (; secondIdx < nums.length; secondIdx++) {
+            if (nums[secondIdx] === target) {
+                // remove num pairs
+                nums.splice(secondIdx, 1)
+                nums.splice(firstIdx, 1)
+                // increase count
+                count++
+                find = true
+                break;
+            }
+        }
+        if (!find) {
+            firstIdx++
+        }
+    }
+
+    return count
+};
+
+// Time complexity O(N) with map
+export function mapSolution(nums: number[], k: number): number {
+    let count = 0
+    const map = new Map()
+
+    nums.forEach(num => {
+        const count = map.get(num) ?? 0
+        map.set(num, count + 1)
+    })
+
+    nums.forEach(num => {
+        const target = k - num
+        const targetCount = map.get(target) ?? 0
+        const numCount = map.get(num) - 1
+        if ((target !== num && targetCount > 0) || (target === num && targetCount > 2)) {
+            map.set(num, numCount - 1)
+            map.set(target, targetCount - 1)
+            count++
+            return
+        }
+    })
+
+    return count
+};
+
+
 
 export function maxOperations(nums: number[], k: number): number {
-    return -1
-};
+    let count = 0;
+    const map = new Map() // A map to store the frequency of each number
+
+    for (let num of nums) {
+        const target = k - num;
+        if (map.get(target) > 0) {
+            // If found, decrement the count of the target value in the map
+            const targetCount = map.get(target) ?? 0
+            map.set(target, targetCount - 1)
+            count++
+        } else {
+            // If target value is not found or its count is 0, increment the count of num in the map
+            const numCount = map.get(num) ?? 0
+            map.set(num, numCount + 1)
+        }
+    }
+
+    return count
+}
+
